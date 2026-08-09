4b8c3ee858faef4885a69ac94bed1030e801d8a7
m4n1kya <m4n1kya2005@gmail.com>
2026-08-09 17:49:02 +0530

    style: Update Navbar logo text to ASHEN

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
     <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
       <div className="inner">
         <a href="#hero" className="logo">
-          Manikya N.
+          ASHEN
         </a>
 
         <nav className="desktop">
