5d92d149ea0aa2f336979dcdb90ec0476b03f58c
m4n1kya <m4n1kya2005@gmail.com>
2026-08-09 17:52:01 +0530

    style: Reduce Navbar logo font size and change casing to Ashen

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
     <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
       <div className="inner">
         <a href="#hero" className="logo">
-          ASHEN
+          Ashen
         </a>
 
         <nav className="desktop">
