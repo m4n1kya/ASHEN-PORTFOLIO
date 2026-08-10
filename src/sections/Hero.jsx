912e389d38f697f423185821cabf8a8a2a2d32b0
m4n1kya <m4n1kya2005@gmail.com>
2026-08-10 22:33:28 +0530

    style: Move scroll down indicator animation up slightly on the Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -103,7 +103,7 @@ const Hero = () => {
       </div>
 
       {/* Aesthetic Animated Scroll Indicator (No Text) */}
-      <div className="absolute bottom-10 left-1/2 -translate-x-1/2 scroll-indicator z-[50] pointer-events-none opacity-50 mix-blend-screen">
+      <div className="absolute bottom-20 left-1/2 -translate-x-1/2 scroll-indicator z-[50] pointer-events-none opacity-50 mix-blend-screen">
         <div className="w-[20px] h-[34px] rounded-full border-[1.5px] border-white flex justify-center p-1 shadow-[0_0_10px_rgba(255,255,255,0.2)]">
           <div className="w-1 h-1 bg-white rounded-full scroll-mouse-dot shadow-[0_0_4px_rgba(255,255,255,1)]"></div>
         </div>
