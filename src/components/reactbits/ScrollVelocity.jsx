fde355b275b4c2881e573eee8e7de195a649aa65
m4n1kya <m4n1kya2005@gmail.com>
2026-08-23 15:01:14 +0530

    style: slightly increase gap between logo marquee rows

--- src/components/reactbits/ScrollVelocity.jsx
+++ src/components/reactbits/ScrollVelocity.jsx
@@ -75,7 +75,7 @@ const ScrollVelocity = ({
   );
 
   return (
-    <div className={`relative flex flex-col gap-4 md:gap-8 ${className}`}>
+    <div className={`relative flex flex-col gap-6 md:gap-12 ${className}`}>
       <ParallaxText baseVelocity={velocity}>{content}</ParallaxText>
       <ParallaxText baseVelocity={-velocity}>{content}</ParallaxText>
     </div>
