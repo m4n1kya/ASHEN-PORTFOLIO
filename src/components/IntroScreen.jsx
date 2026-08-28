1840fc847546d3b90f2164544cf3149106712e0d
m4n1kya <m4n1kya2005@gmail.com>
2026-08-28 23:59:03 +0530

    fix: add missing HeroParticles import in IntroScreen

--- src/components/IntroScreen.jsx
+++ src/components/IntroScreen.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import MaskedHeading from './reactbits/MaskedHeading';
+import HeroParticles from './HeroParticles';
 
 gsap.registerPlugin(ScrollTrigger);
 
