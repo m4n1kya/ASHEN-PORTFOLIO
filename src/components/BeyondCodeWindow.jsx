396c5d8802df05149d8d740be056223e60e32daf
m4n1kya <m4n1kya2005@gmail.com>
2026-08-30 01:21:57 +0530

    feat: replace Galaxy background with interactive Hyperspeed animation in Beyond Code section

--- src/components/BeyondCodeWindow.jsx
+++ src/components/BeyondCodeWindow.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
-import Galaxy from "./reactbits/Galaxy";
+import Hyperspeed from "./reactbits/Hyperspeed";
 import BlobTextReveal from "./reactbits/BlobTextReveal";
 import FlowingMenu from "./reactbits/FlowingMenu";
 
@@ -30,15 +30,44 @@ const BeyondCodeWindow = ({ onBack }) => {
 
   return (
     <div ref={containerRef} className="fixed inset-0 z-[1000] bg-black flex flex-col w-full h-full overflow-hidden">
-      {/* Galaxy Background */}
-      <div className="absolute inset-0 z-0 overflow-hidden pointer-events-none">
-        <Galaxy 
-          mouseRepulsion={true}
-          mouseInteraction={true}
-          density={1.2}
-          glowIntensity={0.2}
-          saturation={0.0}
-          twinkleIntensity={0.4}
+      {/* Hyperspeed Background */}
+      <div className="absolute inset-0 z-0 overflow-hidden pointer-events-auto">
+        <Hyperspeed 
+          effectOptions={{
+            "distortion": "turbulentDistortion",
+            "length": 400,
+            "roadWidth": 10,
+            "islandWidth": 2,
+            "lanesPerRoad": 3,
+            "fov": 90,
+            "fovSpeedUp": 150,
+            "speedUp": 2,
+            "carLightsFade": 0.4,
+            "totalSideLightSticks": 20,
+            "lightPairsPerRoadWay": 40,
+            "shoulderLinesWidthPercentage": 0.05,
+            "brokenLinesWidthPercentage": 0.1,
+            "brokenLinesLengthPercentage": 0.5,
+            "lightStickWidth": [0.12, 0.5],
+            "lightStickHeight": [1.3, 1.7],
+            "movingAwaySpeed": [60, 80],
+            "movingCloserSpeed": [-120, -160],
+            "carLightsLength": [12, 80],
+            "carLightsRadius": [0.05, 0.14],
+            "carWidthPercentage": [0.3, 0.5],
+            "carShiftX": [-0.8, 0.8],
+            "carFloorSeparation": [0, 5],
+            "colors": {
+              "roadColor": 526344,
+              "islandColor": 657930,
+              "background": 0,
+              "shoulderLines": 1250072,
+              "brokenLines": 1250072,
+              "leftCars": [14177983, 6770850, 12732332],
+              "rightCars": [242627, 941733, 3294549],
+              "sticks": 242627
+            }
+          }}
         />
       </div>
 
