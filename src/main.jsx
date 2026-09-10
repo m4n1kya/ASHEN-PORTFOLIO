0fa8897e555e4473ea3f38fc156bc3d4714d0cfd
m4n1kya <m4n1kya2005@gmail.com>
2026-09-10 12:47:27 +0530

    feat: integrate Vercel Analytics for visitor tracking

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import App from './App.jsx'
-
 import ErrorBoundary from './ErrorBoundary.jsx'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
   <ErrorBoundary>
     <App />
+    <Analytics />
   </ErrorBoundary>,
 )
 
