31e39fd4f98e4f90e4bc1abaac3812712de92c9d
m4n1kya <m4n1kya2005@gmail.com>
2026-09-21 00:00:57 +0530

    chore: prepare project for Netlify deployment by adding routing configs and removing Vercel specific analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ErrorBoundary from './ErrorBoundary.jsx'
-import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
   <ErrorBoundary>
     <App />
-    <Analytics />
   </ErrorBoundary>,
 )
 
